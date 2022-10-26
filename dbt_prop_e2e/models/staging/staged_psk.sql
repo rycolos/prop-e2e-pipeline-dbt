459a8f99867c21518acdd0a024243707f68436c9
Ryan LaLiberty <rycolos@gkserver.net>
2022-10-26 12:36:10 -0400

    first psk model

--- dbt_prop_e2e/models/staging/staged_psk.sql
+++ dbt_prop_e2e/models/staging/staged_psk.sql
@@ -1,20 +1,21 @@
---  with source as (
---     select * from {{ source('raw', 'psk') }}
---  ),
+ with source as (
+    select * from {{ source('raw', 'psk') }}
+ ),
 
---  transformed as (
---     select
---         snr,
---         mode as comm_mode,
---         frequency,
---         rxtime as rxtime_utc,
---         senderCallsign as sender_callsign,
---         senderLocator as sender_locator,
---         receiverCallsign as receiver_callsign,
---         receiverLocator as receiver_locator
---  )
+ transformed as (
+    select
+        snr,
+        mode as comm_mode,
+        frequency,
+        rxtime as rxtime_utc,
+        senderCallsign as sender_callsign,
+        senderLocator as sender_locator,
+        receiverCallsign as receiver_callsign,
+        receiverLocator as receiver_locator
+    from source
+ )
 
---  select * from transformed
+select * from transformed
 
 --     id  BIGINT GENERATED ALWAYS AS IDENTITY,
 --     snr INT,
