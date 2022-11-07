9a66e894426d2994f929979c4bf2715ddeeccdd6
Ryan LaLiberty <rycolos@gkserver.net>
2022-11-07 09:07:43 -0500

    updated station_distance macro

--- dbt_prop_e2e/models/marts/psk_logbook_qso.sql
+++ dbt_prop_e2e/models/marts/psk_logbook_qso.sql
@@ -2,7 +2,7 @@ WITH joined_data AS (
 	SELECT
 		p.rxtime_utc as psk_rxtime_utc,
 		l.rxtime_utc as log_rxtime_utc,
-		p.comm_mode,
+		p.comm_mode as comm_mode
 		p.frequency as psk_frequency,
 		l.frequency as log_frequency,
 		p.sender_callsign as sender_callsign,
@@ -11,14 +11,14 @@ WITH joined_data AS (
 		l.rst_sent as log_snr_sent,
 		l.rst_rcvd as log_snr_rcvd
 FROM {{ ref ('fact_psk_contact') }} p
-JOIN {{ ref ('fact_logbook_contact') }}l 
+JOIN {{ ref ('fact_logbook_contact') }} l 
 ON p.receiver_callsign = l.receiver_callsign
 )
 
 SELECT
 	psk_rxtime_utc,
 	log_rxtime_utc,
-	p.comm_mode,
+	comm_mode,
 	psk_frequency,
 	log_frequency,
 	sender_callsign,
