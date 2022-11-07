fd3f8173f953111b65ceb18c8330d7e16889c886
Ryan LaLiberty <rycolos@gkserver.net>
2022-11-07 09:10:49 -0500

    updated station_distance macro

--- dbt_prop_e2e/macros/station_distance.sql
+++ dbt_prop_e2e/macros/station_distance.sql
@@ -1,9 +1,3 @@
--- UPDATE pskreporter_staged
--- SET distance_mi = CAST(SQRT(POW(69.1 * (sender_lat -  receiver_lat), 2) + POW(69.1 * (receiver_lon - sender_lon) * COS(sender_lat / 57.3), 2)) AS REAL)
--- WHERE distance_mi IS NULL;
-
---lat lon from psk_lat and psk_lon in dim_station
-
 {% macro station_distance(station1, station2) -%}
 
 {% set s1_lat_query %}
