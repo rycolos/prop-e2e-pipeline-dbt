bc2f372e85ec08d1641f56e6e7c4fe364d19d595
Ryan LaLiberty <rycolos@gkserver.net>
2022-11-07 09:19:13 -0500

    updated station_distance macro

--- dbt_prop_e2e/macros/station_distance.sql
+++ dbt_prop_e2e/macros/station_distance.sql
@@ -20,10 +20,15 @@ select psk_lon from {{ ref('dim_station') }}
 where callsign = '{{ station2 }}'
 {% endset %}
 
-{% set station1_lat = run_query(s1_lat_query) %}
-{% set station1_lon = run_query(s1_lon_query) %}
-{% set station2_lat = run_query(s2_lat_query) %}
-{% set station2_lon = run_query(s2_lon_query) %}
+{% set station1_latq = run_query(s1_lat_query) %}
+{% set station1_lonq = run_query(s1_lon_query) %}
+{% set station2_latq = run_query(s2_lat_query) %}
+{% set station2_lonq = run_query(s2_lon_query) %}
+
+{% set station1_lat = station1_latq.columns[0].values() %}
+{% set station1_lon = station1_lonq.columns[0].values() %}
+{% set station2_lat = station2_latq.columns[0].values() %}
+{% set station2_lon = station2_lonq.columns[0].values() %}
 
 CAST(SQRT(POW(69.1 * ({{ station1_lat }}::REAL -  {{ station2_lat }}::REAL), 2) + POW(69.1 * ({{ station2_lon }}::REAL - {{ station1_lon }}::REAL) * COS({{ station1_lat }}::REAL / 57.3), 2)) AS REAL)
 
--- dbt_prop_e2e/models/marts/distance_ex.sql
+++ dbt_prop_e2e/models/marts/distance_ex.sql
@@ -2,3 +2,5 @@
 SELECT
     {{ station_distance(KC1QBY, NS4J) }} as distance_mi
 FROM {{ ref('psk_logbook_qso') }}
+
+--NOT WORKING BECAUSE MACRO IS RETURNING A TABLE AND NOT INDIVIDUAL RESULT
