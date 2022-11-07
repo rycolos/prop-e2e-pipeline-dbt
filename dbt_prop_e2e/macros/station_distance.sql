06eb137adea315eb6e4e9e5acd344d07ea069d16
Ryan LaLiberty <rycolos@gkserver.net>
2022-11-07 10:05:26 -0500

    updated station_distance macro

--- dbt_prop_e2e/macros/station_distance.sql
+++ dbt_prop_e2e/macros/station_distance.sql
@@ -1,36 +1,29 @@
 {% macro station_distance(station1, station2) -%}
 
-{% set s1_lat_query %}
-select psk_lat from {{ ref('dim_station') }}
-where callsign = '{{ station1 }}'
-{% endset %}
+WITH station1 AS (
+    SELECT
+        callsign AS s1_call,
+        psk_lat AS s1_lat,
+        psk_lon AS s1_lon
+    FROM {{ ref('dim_station') }}
+    WHERE callsign = {{ station1 }}
+),
 
-{% set s1_lon_query %}
-select psk_lon from {{ ref('dim_station') }}
-where callsign = '{{ station1 }}'
-{% endset %}
+station2 AS (
+    SELECT
+        callsign AS s2_call,
+        psk_lat AS s2_lat,
+        psk_lon AS s2_lon
+    FROM {{ ref('dim_station') }}
+    WHERE callsign = {{ station2 }}
+)
 
-{% set s2_lat_query %}
-select psk_lat from {{ ref('dim_station') }}
-where callsign = '{{ station2 }}'
-{% endset %}
-
-{% set s2_lon_query %}
-select psk_lon from {{ ref('dim_station') }}
-where callsign = '{{ station2 }}'
-{% endset %}
-
-{% set station1_latq = run_query(s1_lat_query) %}
-{% set station1_lonq = run_query(s1_lon_query) %}
-{% set station2_latq = run_query(s2_lat_query) %}
-{% set station2_lonq = run_query(s2_lon_query) %}
-
-{% set station1_lat = station1_latq.columns[0].values() %}
-{% set station1_lon = station1_lonq.columns[0].values() %}
-{% set station2_lat = station2_latq.columns[0].values() %}
-{% set station2_lon = station2_lonq.columns[0].values() %}
-
-CAST(SQRT(POW(69.1 * ({{ station1_lat }}::REAL -  {{ station2_lat }}::REAL), 2) + POW(69.1 * ({{ station2_lon }}::REAL - {{ station1_lon }}::REAL) * COS({{ station1_lat }}::REAL / 57.3), 2)) AS REAL)
+SELECT
+	s1.s1_call,
+	s2.s2_call,
+	CAST(SQRT(POW(69.1 * (s1.s1_lat -  s2.s2_lat), 2) + POW(69.1 * (s2.s2_lon - s1.s1_lon) * COS(s1.s1_lat / 57.3), 2)) AS REAL) as distance_mi
+FROM station1 s1 
+CROSS JOIN station2 s2
 
 {%- endmacro %}
 
