cc1566c31c9a29e4e18574cd8fbce1aaad05e813
Ryan LaLiberty <rycolos@gkserver.net>
2022-11-08 11:58:21 -0500

    updated makefile + docker compose

--- dbt_prop_e2e/models/staged/stg_logbook.sql
+++ dbt_prop_e2e/models/staged/stg_logbook.sql
@@ -6,18 +6,18 @@ pre_transform AS (
 	SELECT
 		cast(qso_date AS text),
 		cast(time_off AS text),
-	    mode as comm_mode,
-      	cast(freq as double precision) as frequency,
-      	station_callsign as home_station_callsign,
-      	my_gridsquare as home_station_locator,
-      	my_country as home_station_country,
-      	call as receiver_callsign,
-      	gridsquare as receiver_locator,
-      	country as receiver_country,
-      	rst_rcvd,
-      	rst_sent,
-      	tx_pwr,
-      	cast(app_qrzlog_logid as bigint),
+	   mode as comm_mode,
+      cast(freq as double precision) as frequency,
+      station_callsign as home_station_callsign,
+      my_gridsquare as home_station_locator,
+      my_country as home_station_country,
+      call as receiver_callsign,
+      gridsquare as receiver_locator,
+      country as receiver_country,
+      rst_rcvd,
+      rst_sent,
+      tx_pwr,
+      cast(app_qrzlog_logid as bigint),
 		cast(qrzcom_qso_upload_date as text)
 	FROM source
 ),
@@ -34,12 +34,32 @@ transformed as (
       receiver_callsign,
       receiver_locator,
       receiver_country,
-      cast(rst_rcvd as int),
-      cast(rst_sent as int),
-      cast(tx_pwr as int),
+      rst_rcvd,
+      rst_sent,
+      tx_pwr,
       cast(app_qrzlog_logid as bigint),
       cast(qrzcom_qso_upload_date as date)
     from pre_transform
+),
+
+final as (
+    select
+      {{ dbt_utils.surrogate_key(['rxtime_utc', 'sendercallsign', 'receivercallsign']) }} as id,
+      rxtime_utc,
+      comm_mode,
+      frequency,
+      home_station_callsign,
+      home_station_locator,
+      home_station_country,
+      receiver_callsign,
+      receiver_locator,
+      receiver_country,
+      rst_rcvd,
+      rst_sent,
+      tx_pwr,
+      app_qrzlog_logid,
+      qrzcom_qso_upload_date
+    from transformed
 )
 
-select * from transformed
+select * from final
