c2a7921dfac039a6e0a88034250232c642c89108
Ryan LaLiberty <rycolos@gkserver.net>
2022-11-08 11:36:25 -0500

    updated makefile + docker compose

--- dbt_prop_e2e/models/staged/stg_logbook.sql
+++ dbt_prop_e2e/models/staged/stg_logbook.sql
@@ -2,24 +2,43 @@
     select * from {{ source('source', 'logbook') }}
  ),
 
- transformed as (
+WITH pre_transform AS (
+	SELECT
+		cast(qso_date AS text),
+		cast(time_off AS text),
+	    mode as comm_mode,
+      	cast(freq as double precision) as frequency,
+      	station_callsign as home_station_callsign,
+      	my_gridsquare as home_station_locator,
+      	my_country as home_station_country,
+      	call as receiver_callsign,
+      	gridsquare as receiver_locator,
+      	country as receiver_country,
+      	rst_rcvd,
+      	rst_sent,
+      	tx_pwr,
+      	cast(app_qrzlog_logid as bigint),
+		cast(qrzcom_qso_upload_date as text)
+	FROM source
+),
+
+transformed as (
     select
-      {{ dbt_utils.surrogate_key(['qso_date', 'time_off', 'call']) }} as id,
       to_timestamp(cast(extract(epoch from(cast(qso_date as date) + make_time(cast(substring(time_off, 1, 2) as int), cast(substring(time_off, 3, 2) as int), 00))) as double precision)) as rxtime_utc,
-      mode as comm_mode,
-      cast(frequency as double precision),
-      station_callsign as home_station_callsign,
-      my_gridsquare as home_station_locator,
-      my_country as home_station_country,
-      call as receiver_callsign,
-      gridsquare as receiver_locator,
-      country as receiver_country,
+      comm_mode,
+      frequency,
+      home_station_callsign,
+      home_station_locator,
+      home_station_country,
+      receiver_callsign,
+      receiver_locator,
+      receiver_country,
       cast(rst_rcvd as int),
       cast(rst_sent as int),
       cast(tx_pwr as int),
       cast(app_qrzlog_logid as bigint),
       cast(qrzcom_qso_upload_date as date)
-    from source
- )
+    from pre_transform
+)
 
 select * from transformed
