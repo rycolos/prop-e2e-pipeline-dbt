e9b804fe125914306555ea78f2f595cdc181969a
Ryan LaLiberty <rycolos@gkserver.net>
2022-11-08 09:19:28 -0500

    updated makefile

--- sql/latlon_to_distance.sql
+++
@@ -1,3 +0,0 @@
-UPDATE pskreporter_staged
-SET distance_mi = CAST(SQRT(POW(69.1 * (sender_lat -  receiver_lat), 2) + POW(69.1 * (receiver_lon - sender_lon) * COS(sender_lat / 57.3), 2)) AS REAL)
-WHERE distance_mi IS NULL;
