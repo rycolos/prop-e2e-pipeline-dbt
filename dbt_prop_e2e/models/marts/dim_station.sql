6a99d5447562c690b7cc4920cc73ca0ed46c05c3
Ryan LaLiberty <rycolos@gkserver.net>
2022-11-11 06:40:53 -0500

    updates to columns in marts

--- dbt_prop_e2e/models/marts/dim_station.sql
+++ dbt_prop_e2e/models/marts/dim_station.sql
@@ -59,7 +59,8 @@ final_merge AS (
 
 transformed AS ( 
     SELECT 
-        callsign,
+        {{ dbt_utils.surrogate_key(['callsign, 'psk_locator,']) }} as id,
+		callsign,
         psk_locator,
         logb_locator,
         psk_country,
